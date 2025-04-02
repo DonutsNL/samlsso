7a2c3e0ab849ecd2f494adeed69447ad97c1b8a4
DonutsNL <chris.gralike@flevo-scouts.nl>
2025-04-02 11:33:26 +0200

    Add initial

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'donutsnl/glpisaml',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7f33b9f55273687b2d33e77512701256c98caad4',
+        'type' => 'glpi-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'donutsnl/glpisaml' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7f33b9f55273687b2d33e77512701256c98caad4',
+            'type' => 'glpi-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'onelogin/php-saml' => array(
+            'pretty_version' => '4.2.0',
+            'version' => '4.2.0.0',
+            'reference' => 'd3b5172f137db2f412239432d77253ceaaa1e939',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../onelogin/php-saml',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'robrichards/xmlseclibs' => array(
+            'pretty_version' => '3.1.3',
+            'version' => '3.1.3.0',
+            'reference' => '2bdfd742624d739dfadbd415f00181b4a77aaf07',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../robrichards/xmlseclibs',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
