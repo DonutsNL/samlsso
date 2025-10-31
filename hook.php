184c7eb881e4c53d75e374495f591b175757c0e0
DonutsNL <chris.gralike@quinquies.nl>
2025-10-31 12:44:45 +0100

    Bump version for testing and releasing.

--- hook.php
+++ hook.php
@@ -32,7 +32,7 @@
  * ------------------------------------------------------------------------
  *
  *  @package    samlSSO
- *  @version    1.2.1
+ *  @version    1.2.2
  *  @author     Chris Gralike
  *  @copyright  Copyright (c) 2024 by Chris Gralike
  *  @license    GPLv3+
