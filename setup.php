ae445f89155b8f7b5be0d5a1208600e33d0beb6f
E <2974895+eduardomozart@users.noreply.github.com>
2026-05-30 19:05:06 -0300

    Update setup.php

--- setup.php
+++ setup.php
@@ -124,8 +124,9 @@ function plugin_init_samlsso(): void                                            
 
     if ($is_login_post && !$is_bypassed) {
         if (Config::getIsEnforced()) {
-            // Nullify the password payload so GLPI native authentication fails
-            $_POST['login_password'] = 'invalid_password_due_to_sso_enforcement';
+            // Nullify the password payload securely so GLPI native authentication fails
+            $random_pass = bin2hex(random_bytes(32));
+            $_POST['login_password'] = $random_pass;
         }
     }
 
